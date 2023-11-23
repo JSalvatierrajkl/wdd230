a8919100e12f3807fbaa30b567e5813b54c0eb04
JSalvatierrajkl <joeljsalvatierra@gmail.com>
2023-11-23 00:09:31 -0300

    a

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,6 @@ document.getElementById("joinus").addEventListener("click", function() {
 	window.location.href = "join.html";
 });
 
-// Get the current year and populate it in the footer's first paragraph
-const currentYear = new Date().getFullYear();
-document.getElementById('currentYear').textContent = currentYear;
 
 // Get the last modified date and populate it in the second paragraph
 const lastModified = document.lastModified;
